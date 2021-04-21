23febc2f5a0e8f6bc82de171f2147f28450a561d
EC2 Default User <ec2-user@ip-172-31-61-5.ec2.internal>
2021-04-21 12:08:51 +0000

    [Finish]5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '996f3ae8e4646a7babedad0f578ad068213a99749fc776e146aeb3f0f431e2e413e74e21061826414f147001002e80f0da9164cfe208e3b2385ad5672ac9ca72'
