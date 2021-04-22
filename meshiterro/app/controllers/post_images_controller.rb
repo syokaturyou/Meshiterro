cc7ed6b9e4e8d3389b3ea2fbedfaa847c8585674
EC2 Default User <ec2-user@ip-172-31-61-5.ec2.internal>
2021-04-22 16:26:09 +0000

    [Finish]5-6

--- meshiterro/app/controllers/post_images_controller.rb
+++ meshiterro/app/controllers/post_images_controller.rb
@@ -4,32 +4,36 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
-  # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path  end
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+
+  end
 
   def index
-    @post_images = PostImage.page(params[:page]).reverse_order
+     @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
-    @post_image = PostImage.find(params[:id])
-    @post_comment = PostComment.new
+     @post_image = PostImage.find(params[:id])
+     @post_comment = PostComment.new
   end
 
   def destroy
-    @post_image = PostImage.find(params[:id])
-    @post_image.destroy
-    redirect_to post_images_path
+     @post_image = PostImage.find(params[:id])
+     @post_image.destroy
+     redirect_to post_images_path
   end
 
-  # 投稿データのストロングパラメータ
   private
 
   def post_image_params
-    params.require(:post_image).permit(:shop_name, :image, :caption)
+     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
+
 end
